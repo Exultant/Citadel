2a5aace067c8f16fdcdc368db4fe8edb441dc494
fndragon <fndragon@gmail.com>
2013-04-02 02:10:50 -0400

    Merged the listeners together so we only have one listener.  Also added check for PhysicalShop

--- src/com/untamedears/citadel/listener/BlockListener.java
+++ src/com/untamedears/citadel/listener/BlockListener.java
@@ -13,6 +13,7 @@ import static com.untamedears.citadel.Utility.sendThrottledMessage;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -53,44 +54,34 @@ import com.untamedears.citadel.entity.ReinforcementMaterial;
 public class BlockListener implements Listener {
 
     /**
-     * This handles the BlockPlaceEvent to disallow placing signs directly above unaccessable chests
-     *
-     * @param bpe BlockPlaceEvent
-     */
-	@EventHandler(ignoreCancelled = true)
-	public void onBlockPlace( BlockPlaceEvent bpe ) {
-		// See if block is a sign.
-		Block block = bpe.getBlockPlaced();
-		if( block.getType() != Material.WALL_SIGN ) {
-			return;
-		}
-		// See if block below is a chest
-		Block below = block.getRelative(0, -1, 0);
-		if( below.getType() != Material.CHEST ) {
-			return;
-		}
-
-		// Finally check and see if the chest is reinforced.
-        IReinforcement reinforcement = AccessDelegate.getDelegate(below).getReinforcement();
-        PlayerReinforcement pr = (PlayerReinforcement)reinforcement;
-        // Return if this is a natural reinforcement
-        if( null == pr ) {
-        	return;
-        }
-
-        // And cancel the event (sign placement) if the user can't access the reinforced chest below
-    	if( false == pr.isAccessible(bpe.getPlayer().getName()) ) {
-    		bpe.setCancelled( true );
-    	}
-	}
-	
-    /**
      * This handles the BlockPlaceEvent for Fortification mode (all placed blocks are reinforced)
      *
      * @param bpe BlockPlaceEvent
      */
     @EventHandler(ignoreCancelled = true)
     public void placeFortifiedBlock(BlockPlaceEvent bpe) {
+    	// Do an initial check to see if we need to worry about physical shop interaction.
+    	if( Bukkit.getPluginManager().isPluginEnabled("PhysicalShop")) {
+    		// See if block is a sign.
+    		Block block = bpe.getBlockPlaced();
+    		if(block.getType() == Material.WALL_SIGN ) {
+        		Block below = block.getRelative(0, -1, 0);
+	    		if(below.getType() == Material.CHEST) {
+	    		    IReinforcement reinforcement = AccessDelegate.getDelegate(below).getReinforcement();
+	    		    if( null != reinforcement ) {
+		                if( reinforcement instanceof PlayerReinforcement ) {
+		                    PlayerReinforcement pr = (PlayerReinforcement)reinforcement;
+		                	if( false == pr.isAccessible( bpe.getPlayer().getName())) {
+		                		bpe.setCancelled( true );
+		                		// we're done here.
+		                		return;
+		                	}
+		                }
+	    		    }
+	    		}
+    		}
+    	}
+    	
         Player player = bpe.getPlayer();
         PlayerState state = PlayerState.get(player);
         Faction group = state.getFaction();
